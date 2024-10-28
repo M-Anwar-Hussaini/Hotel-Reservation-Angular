3a6472a21855ee6f2950e2a3688b33d7be7f7a60
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-10-28 22:05:26 +0430

    Add reservation form

--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reservation-form',
   templateUrl: './reservation-form.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class ReservationFormComponent {
-
+  reservationForm: FormGroup = new FormGroup({});
+  onSubmit() {}
 }
