e21d99b402251054625c1e360c68705edcc3e932
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-10-28 21:16:29 +0430

    Create the module one

---
+++ src/app/reservation-list/reservation-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reservation-list',
+  templateUrl: './reservation-list.component.html',
+  styles: [
+  ]
+})
+export class ReservationListComponent {
+
+}
