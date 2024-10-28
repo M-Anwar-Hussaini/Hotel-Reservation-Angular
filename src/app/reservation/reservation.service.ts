171ce6b6b1e44e287a132cf1e94387e5f51dc38c
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-10-28 22:57:37 +0430

    Add crud operations to the reservation service

--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { Reservation } from '../models/reservation';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ReservationService {
+  private reservations: Reservation[] = [];
 
-  constructor() { }
+  getAllReservations(): Reservation[] {
+    return this.reservations;
+  }
+
+  getSingleReservation(id: string): Reservation | undefined {
+    return this.reservations.find((r) => r.id === id);
+  }
+
+  addReservation(reservation: Reservation): void {
+    this.reservations.push(reservation);
+  }
+
+  deleteReservation(id: string): void {
+    this.reservations = this.reservations.filter((r) => r.id !== id);
+  }
+
+  updateReservation(reservation: Reservation): void {
+    const index = this.reservations.findIndex((r) => r.id === reservation.id);
+    this.reservations[index] = reservation;
+  }
 }
